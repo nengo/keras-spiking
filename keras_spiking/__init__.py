3d68f1e5789202a81c2b968a09b1f99b9e88a567
Eric Hunsberger <eric.hunsberger@appliedbrainresearch.com>
2021-01-04 10:41:54 -0500

    Update copyright dates to 2021

--- keras_spiking/__init__.py
+++ keras_spiking/__init__.py
@@ -1,6 +1,6 @@
 # pylint: disable=missing-docstring
 
-__copyright__ = "2020-2020, Applied Brain Research"
+__copyright__ = "2020-2021, Applied Brain Research"
 __license__ = "Free for non-commercial use; see LICENSE.rst"
 from keras_spiking import callbacks, layers, regularizers
 from keras_spiking.config import default
