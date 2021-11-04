2ea45d877eb9f9b427d64c5f91ea98e406914733
Daniel Rasmussen <daniel.rasmussen@appliedbrainresearch.com>
2021-11-04 16:00:04 -0300

    Use NengoBones to template version.py

--- keras_spiking/__init__.py
+++ keras_spiking/__init__.py
@@ -1,6 +1,5 @@
 # pylint: disable=missing-docstring
 
-__copyright__ = "2020-2021, Applied Brain Research"
 __license__ = "Free for non-commercial use; see LICENSE.rst"
 from keras_spiking import callbacks, constraints, layers, model_energy, regularizers
 from keras_spiking.config import default
@@ -13,4 +12,5 @@ from keras_spiking.layers import (
     SpikingActivationCell,
 )
 from keras_spiking.model_energy import ModelEnergy
+from keras_spiking.version import copyright as __copyright__
 from keras_spiking.version import version as __version__
